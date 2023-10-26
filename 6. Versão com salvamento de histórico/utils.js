222d7df30562274669a8d8551de1cb1a3b8835fe
João Vítor F. B <jvbeltrame@inf.ufsm.br>
2023-10-26 19:16:49 -0300

    histórico implementado

---
+++ 6. Versão com salvamento de histórico/utils.js
@@ -0,0 +1,36 @@
+function isNullString(value) {
+    if (value == "null") {
+      return;
+    } else {
+      return value;
+    }
+}
+
+function getRandExURL(userId = null, dificuldade = "iniciante") {
+    var url = "https://script.google.com/macros/s/AKfycbwZKg-bWoZs_OgVkRUmvxxfrdQeSTWbk3lANkRDUPik-zAvLWfieRkhCgFrU415LYYg/exec?actionRequest=getExercicioAleatorio";
+
+    // Add dificuldade
+    if (userId == null) {
+        // NÃO FUNCIONA AINDA SEM ESTAR LOGADO
+        url = url + "&dificuldade=" + dificuldade;
+    } else {
+        // Usuário está logado
+        url = url + "&dificuldade=userDefault";
+        url = url + "&userId=" + userId;
+    }
+
+    return url;
+}
+
+
+function getCookie(cName) {
+    const name = cName + "=";
+    const cDecoded = decodeURIComponent(document.cookie); //to be careful
+    const cArr = cDecoded.split('; ');
+    let res;
+    cArr.forEach(val => {
+        if (val.indexOf(name) === 0) res = val.substring(name.length);
+    });
+    res = JSON.parse(res);
+    return res;
+}
